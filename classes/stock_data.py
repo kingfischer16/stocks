6a2525d034d47c597f88c06c3cb2e571f71a3731
Lee <lee.m.fischer@gmail.com>
2020-03-18 17:07:55 +0100

    created folders and file headers for classes and functions

---
+++ classes/stock_data.py
@@ -0,0 +1,16 @@
+"""
+# ============================================================================
+# STOCK_DATA.PY
+# ----------------------------------------------------------------------------
+# Contains the 'StockData' class and associated methods for importing
+# stock data using the 'yfinance' package, as well as calculating
+# metrics and averaging data.
+#
+# ============================================================================
+"""
+
+# Imports.
+import os
+import pandas as pd
+import yfinance as yf
+
---
+++ plotting/plotting.py
@@ -0,0 +1,14 @@
+"""
+# ============================================================================
+# PLOTTING.PY
+# ----------------------------------------------------------------------------
+# As the extremely original name suggests, this contains some plotting
+# functions for stock data. Plots are created with the 'altair' package
+# and are interactive.
+#
+# ============================================================================
+"""
+
+# Imports.
+import pandas as pd
+import altair as alt
